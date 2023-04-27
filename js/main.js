6f3e14cddb5f2343bb66c8ce2bde8f461e37b7b0
Emil <emil19@users.noreply.github.com>
2023-04-27 21:31:10 +0200

    initial version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+//
+// Place any custom JS here
+//
